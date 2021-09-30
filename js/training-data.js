7241713478379394b5197e8d719c3599c43b5ef8
saeed-tajik <saeid.tajik@gmail.com>
2021-09-30 13:26:08 +0330

    Init

---
+++ js/training-data.js
@@ -0,0 +1,233 @@
+var treeData = [
+  {
+    "name": "آموزش پایتون",
+    "parent": "null",
+    "children": [
+      {
+        "name": "گام اول - مقدماتی"
+        ,"children": [
+          {
+            "name": "قسمت اول : نصب",
+            "parent": "گام اول - مقدماتی",
+            "_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/aadgZ"
+              }
+ 
+            ]
+         },
+          {
+            "name": "قسمت دوم : تعریف متغیر",
+            "_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/CzE1h"
+              }
+ 
+            ]
+ 
+          },
+          {
+            "name": "Block Indentation : قسمت سوم"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/b9v1P"
+              }
+            ]
+
+          },
+          {
+            "name": "قسمت چهارم : انواع داده"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/VcTeG"
+              }
+ 
+            ]
+ 
+          },
+          {
+            "name": "قسمت پنجم : تبدیل داده ها به یکدیگر"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/ryXv8"
+              }
+ 
+            ]
+          },
+          {
+             "name": "قسمت ششم : ماژول خودتو بساز"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/tu7Ud"
+              }
+ 
+            ]
+ 
+          },
+          {
+             "name": "قسمت هفتم : کامنت و داکیومنت"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/3myfI"
+              }
+ 
+            ]
+ 
+          },
+          {
+             "name": "قسمت هشتم : عبارات شرطی"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/mmz7T"
+              }
+ 
+            ]
+ 
+         },
+          {
+             "name": "قسمت نهم : حلقه ها بخش اول"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/FzPXH"
+              }
+ 
+            ]
+ 
+         },
+          {
+             "name": "قسمت دهم : حلقه ها بخش دوم"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/QS0Js"
+              }
+ 
+            ]
+ 
+         },
+          {
+             "name": "قسمت یازدهم : عملگرها"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/Ldqrs"
+              }
+ 
+            ]
+ 
+         },
+          {
+             "name": "جمع بندی گام اول"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/OISdV"
+              }
+ 
+            ]
+ 
+         }
+
+        ]
+      },
+      {
+        "name": "گام دوم - سطح متوسط"
+        ,"_children": [
+          {
+            "name": "قسمت دوازدهم : مجموعه ها"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/xlp7F"
+              }
+ 
+            ]
+
+          },
+          {
+            "name": "قسمت سیزدهم : تاپل ها"
+            ,"_children": [
+              {  
+                "name":   "مشاهده درس",
+                 "url": "http://vrgl.ir/B3pjN"
+              }
+ 
+            ]
+          },
+          {
+            "name": "قسمت چهاردهم : لیست ها قسمت اول"
+            ,"_children": [
+              {
+                "name":   "مشاهده درس",
+                 "url": "https://vrgl.ir/64Lof"
+              }
+
+            ]
+          },
+          {
+            "name": "قسمت پانزدهم : لیست ها قسمت دوم"
+            ,"_children": [
+              {
+                "name":   "مشاهده درس",
+                 "url": "https://vrgl.ir/BNHid"
+              }
+
+            ]
+          },
+          {
+            "name": "قسمت شانزدهم : دیکشنری ها"
+            ,"_children": [
+              {
+                "name":   "مشاهده درس",
+                 "url": "https://vrgl.ir/FiRvF"
+              }
+
+            ]
+          },
+          {
+            "name": "قسمت هفدهم : کار با توابع"
+            ,"_children": [
+              {
+                "name":   "مشاهده درس",
+                 "url": "https://vrgl.ir/6krc5"
+              }
+
+            ]
+          },
+          {
+            "name": "قسمت هجدهم : کار با فایل"
+            ,"_children": [
+              {
+                "name":   "مشاهده درس",
+                 "url": "https://vrgl.ir/0gITL"
+              }
+
+            ]
+          },
+          {
+            "name": "جمع بندی گام دوم"
+            ,"_children": [
+              {
+                "name":   "مشاهده درس",
+                 "url": "https://vrgl.ir/bAoin"
+              }
+
+            ]
+          }
+
+        ]
+
+      }
+    ]
+  }
+];
+
